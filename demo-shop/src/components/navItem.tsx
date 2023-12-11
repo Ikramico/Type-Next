62c403db16a5c3b0cc502846d67cadae56337da6
Ikram Ico <ikramicono@gmail.com>
2023-12-11 17:41:19 +0600

    nav cont

--- demo-shop/src/components/navItem.tsx
+++ demo-shop/src/components/navItem.tsx
@@ -30,9 +30,9 @@ return ( <div className="flex">
             <div className={cn("absolute inset-x-0 top-full text-sm text-muted-foreground",{
                 'animate-in fade-in-10 slide-in-from-top-5':!isAnyOpen,
             })}>
-                <div className="absolute inset-0 top-1/2 bg-white shadow" aria-hidden='true' />
+                <div className="absolute inset-0 top-1/2 bg-white shadow " aria-hidden='true' />
                 <div className="relative bg-white">
-                    <div className="max-auto max-w-7xl px-8">
+                    <div className="mx-auto max-w-7xl px-8">
                         <div className="grid grid-cols-4 gap-x-8 gap-y-10 py-16">
                             <div className="col-span-4 col-start-1 grid grid-cols-3 gap-x-8">
                                 {
--- demo-shop/src/components/navItems.tsx
+++ demo-shop/src/components/navItems.tsx
@@ -1,14 +1,33 @@
 "use client"
 
-import { useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import { categories } from "../config"
 import NavItem from "./navItem"
+import { useOnClickOutside } from "../hooks/outClick"
 
 const NavItems =()=>{
     const[activeIndex, setActiveIndex] = useState<null | number>(null)
     const isAnyOpen = activeIndex !== null;
 
-    return <div className="flex gap-4 full">
+    const navRef = useRef<HTMLDivElement | null>(null);
+
+    useEffect(()=>{
+        const handler =(e: KeyboardEvent)=>{
+            if(e.key === 'Escape'){
+                setActiveIndex(null)
+            }
+        }
+        document.addEventListener('keydown', handler);
+
+        return()=>{
+            document.removeEventListener('keydown', handler);
+        }
+    },[])
+
+    useOnClickOutside(navRef, ()=>{
+        setActiveIndex(null)
+    });
+    return <div className="flex gap-4 full" ref={navRef}>
         {categories.map((category, i)=>{
 
             const handleOpen =() =>{
