ddb2e6ad5fd11e82326afd7b05b5ae2a3c513308
Ikram Ico <ikramicono@gmail.com>
2023-12-17 22:37:07 +0600

    dashboard cont

--- demo-shop/src/payload.config.ts
+++ demo-shop/src/payload.config.ts
@@ -1,12 +1,29 @@
 import { buildConfig } from 'payload/config'
 import {mongooseAdapter} from '@payloadcms/db-mongodb';
 import { slateEditor } from '@payloadcms/richtext-slate';
+import { webpackBundler } from '@payloadcms/bundler-webpack';
 
 export default buildConfig({
     db: mongooseAdapter({
         url: process.env.MONGODB_URL!,
     }),
     editor: slateEditor({}),
+    admin:{
+        bundler: webpackBundler(),
+        meta:{
+            titleSuffix: '-blocks',
+            favicon: '/favicon.ico',
+            ogImage: '/thumbnail,jpg'
+        }
+    },
+    rateLimit: {
+        max: 2000
+    },
+    serverURL: process.env.NEXT_PUBLIC_SERVER_URL || '',
+    collections: [],
+    routes:{
+        admin: '/sell'
+    }
 })
 
 
