8b859eee9a0048f244e2758be75226543ab25037
Ikram Ico <ikramicono@gmail.com>
2023-12-27 22:28:14 +0600

    auth cont

--- demo-shop/src/payload.config.ts
+++ demo-shop/src/payload.config.ts
@@ -4,7 +4,12 @@ import { slateEditor } from '@payloadcms/richtext-slate';
 import { webpackBundler } from '@payloadcms/bundler-webpack';
 import path from 'path';
 import { Users } from './collections/users';
+import dotenv from 'dotenv'
 
+
+dotenv.config({
+    path: path.resolve(__dirname, '../.env'),
+})
 export default buildConfig({
     db: mongooseAdapter({
         url: process.env.MONGODB_URL!,
