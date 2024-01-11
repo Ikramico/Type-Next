e4bd3362ddda549424f71547ad7a3c90543fed0b
Ikram Ico <ikramicono@gmail.com>
2024-01-11 22:36:28 +0600

    update

--- demo-shop/src/app/(auth)/verification-email/page.tsx
+++ demo-shop/src/app/(auth)/verification-email/page.tsx
@@ -6,6 +6,7 @@ interface PageProps{
 
 const VerifyEmailPage =({searchParams}: PageProps) =>{
     const token = searchParams.token;
+    const toEmail = searchParams.to;
     return (
         <div className="container relative flex pt-20 flex-col items-center justify-center lg:px-0">
             <div className="mx-auto fulex w-full flex-col justify-center sm:w-[350px]">
