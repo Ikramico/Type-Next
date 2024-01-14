8bbdb904c7d302cb733dc72c88e06174036cddb0
Ikram Ico <ikramicono@gmail.com>
2024-01-14 23:02:26 +0600

    mail verification cont

--- demo-shop/src/components/verifyEmail.tsx
+++ demo-shop/src/components/verifyEmail.tsx
@@ -1,7 +1,9 @@
 'use client'
 
-import { XCircle } from "lucide-react"
+import { Loader2, XCircle } from "lucide-react"
 import { trpc } from "../trpc/client"
+import Link from "next/link"
+import { buttonVariants } from "./ui/button"
 
 interface VerifyEmailProps{
     token: string
@@ -29,9 +31,26 @@ const VerifyEmail =({token} : VerifyEmailProps) =>{
                 <h3 className="font-semibold text-2xl">
                     You&apos;re all set!
                 </h3>
+                <p className="text-muted-foreground text-center mt-1">
+                    Thank you for your mail.
+                </p>
+                <Link className={buttonVariants({className: 'mt-4'})} href='/sign-in'>
+                    Sign in
+                </Link>
             </div>
         )
 
+    }
+    if(isLoading){
+        return(
+            <div className="flex flex-col items-center justify-center gap-2">
+                <Loader2 className="animate-spin h-8 w-8 text-zinc-300"/>
+                <h3 className="font-semibold text-xl">Verifying....</h3>
+                <p className="text-muted-foreground text-sm">
+                    This won&apos;t take long.
+                </p>
+            </div>
+        )
     }
 }
 
